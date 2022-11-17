701257951becf0080caa1a4e678eaf6f106f8704
stevematdavies <stevematdavies@gmail.com>
2022-11-17 15:41:37 +0200

    Adding email sending handler

---
+++ src/main/java/com/steve/tutorials/springsecurityexample/email/EmailService.java
@@ -0,0 +1,44 @@
+package com.steve.tutorials.springsecurityexample.email;
+
+
+import lombok.AllArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+@AllArgsConstructor
+public class EmailService implements IEmailSender {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+    private final JavaMailSender mailSender;
+
+
+    private void logErrorMessage(String message){
+
+
+    }
+
+    @Override
+    @Async
+    public void send(String to, String email) throws IllegalStateException{
+        try {
+            MimeMessage mimeMessage = mailSender.createMimeMessage();
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "utf-8");
+            helper.setText(email, true);
+            helper.setTo(to);
+            helper.setSubject("Confirm your email");
+            helper.setFrom("Hello from Steve's Secure");
+
+        } catch (MessagingException e){
+            LOGGER.error("Failed to send email -- ", e);
+            throw new IllegalStateException("Failed to send email");
+        }
+    }
+}
---
+++ src/main/java/com/steve/tutorials/springsecurityexample/email/IEmailSender.java
@@ -0,0 +1,5 @@
+package com.steve.tutorials.springsecurityexample.email;
+
+public interface IEmailSender {
+    void send(String to, String email);
+}
